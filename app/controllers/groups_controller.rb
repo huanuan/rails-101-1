65368536a25ee28733c92bae2ad819b8159b50d2
huanuan <xieping7006@gmail.com>
2016-07-27 13:17:17 +0800

    join or quit group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -29,8 +29,6 @@ class GroupsController < ApplicationController
   def edit
   end
 
-
-
  def create
    @group = Group.new(group_params)
    @group.user = current_user
@@ -45,8 +43,32 @@ class GroupsController < ApplicationController
   def destroy
   @group.destroy
   redirect_to groups_path, alert: "Group deleted"
-end
+  end
 
+  def join
+   @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本討論版成功！"
+    else
+      flash[:warning] = "你已經是本討論版成員了！"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本討論版！"
+    else
+      flash[:warning] = "你不是本討論版成員，怎麼退出 XD"
+    end
+    redirect_to group_path(@group)
+  end
  private
 
  def find_group_and_check_permission
@@ -61,8 +83,4 @@ end
    params.require(:group).permit(:title, :description)
  end
 
-end
-
-def is_member_of?(group)
-   participated_groups.include?(group)
 end
